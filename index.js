b0b940ee89fc72be5a0e331eb4e9514239cacb39
Jose Rivas <joserivas1085@gmail.com>
2025-10-20 22:56:51 -0300

    GET Items/{id}

--- index.js
+++ index.js
@@ -3,17 +3,31 @@
 
 // console.log(method, item);
 
-const [, , method, item] = process.argv;
+let [, , method, item] = process.argv;
 
 // Items
-if (method == 'GET' && item.startsWith("items/")) {
-    const params = item.split("/");
-    console.log(params);
-}
+// if (method == 'GET' && item.startsWith("items/")) {
+//     const params = item.split("/");
+//     console.log(params);
+// }
 
-if (method === 'GET' && item == "item") {
-    fetch('https://fakestoreapi.com/products')
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
+// if (method === 'GET' && item == "item") {
+//     fetch('https://fakestoreapi.com/products')
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+// }
 
+// Get single item
+if (method === 'GET' && item.startsWith("items/")) {
+    // const [, id] = item.split("/");
+    let id = item.split("/")[1];
+    id = parseInt(id);
+
+    if (isNaN(id) || id <= 0) {
+        console.log("Is not a number or it is less than 1");
+    }
+
+    fetch("https://fakestoreapi.com/products/" + id)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+}
