36547f3ebfb922b6768930bfc57a47d59fcb3be4
Jose Rivas <joserivas1085@gmail.com>
2025-10-20 21:50:13 -0300

    Get Items

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+// const method = process.argv[2];
+// const item = process.argv[3];
+
+// console.log(method, item);
+
+const [, , method, item] = process.argv;
+
+// Items
+if (method == 'GET' && item.startsWith("items/")) {
+    const params = item.split("/");
+    console.log(params);
+}
+
+if (method === 'GET' && item == "item") {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
